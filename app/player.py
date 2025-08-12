cbfb21261d3ea7c37e073a398f6a283cb82d625d
LukeMillerCommits <lukemiller6024@gmail.com>
2025-08-12 14:28:12 +0800

    Step 1 and 2 complete

---
+++ app/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, uid, name):
+        self.uid = uid
+        self.name = name
+
+    def uid(self):
+        return self.uid
+
+    def name(self):
+        return self.name
+
+    def __str__(self):
+        return str(self)
---
+++ test/player_test.py
@@ -0,0 +1,12 @@
+from app.player import Player
+import unittest
+from unittest import TestCase
+
+
+class TestNode(unittest.TestCase):
+    def test_uid_returns_uid(self):
+        self.assertEqual(Player(1, "Luke").uid, 1)
+
+    def test_name_returns_name(self):
+        self.assertEqual(Player(1, "Luke").name, "Luke")
+
